a6dc04e30a1fc27737952d96d46a0f16f14d1dea
MegaV0lt <MegaV0lt@users.noreply.github.com>
2021-09-21 08:18:58 +0200

    Update logging

--- mp_logos.sh
+++ mp_logos.sh
@@ -12,7 +12,7 @@
 # Die Logos liegen im PNG-Format und mit 190 Pixel Breite vor
 # Es müssen die Varialen 'LOGODIR' und 'MP_LOGODIR' angepasst werden.
 # Das Skript am besten ein mal pro Woche ausführen (/etc/cron.weekly)
-VERSION=210920
+VERSION=210921
 
 # Sämtliche Einstellungen werden in der *.conf vorgenommen.
 # ---> Bitte ab hier nichts mehr ändern! <---
@@ -42,7 +42,7 @@ f_log(){  # Logausgabe auf Konsole oder via Logger. $1 zum kennzeichnen der Meld
 f_process_channellogo() {  # Verlinken der Senderlogos zu den gefundenen Kanälen
   local CHANNEL_PATH EXT='png' LOGO_FILE
   for var in FILE CHANNEL[*] MODE ; do
-    [[ -z "${!var}" ]] && { f_log "Fehler: Variable $var ist nicht gesetzt!" ; exit 1 ;}
+    [[ -z "${!var}" ]] && { f_log ERR "Variable $var ist nicht gesetzt!" ; exit 1 ;}
   done
   if [[ "$USE_SVG" == 'true' ]] ; then  # Die Originalen *.svg-Logos verwenden
     EXT='svg'  # Erweiterung der Logo-Datei
@@ -51,14 +51,14 @@ f_process_channellogo() {  # Verlinken der Senderlogos zu den gefundenen Kanäle
     else
       LOGO_FILE="${MP_LOGODIR}/${MODE}/${FILE%.*} - Dark.${EXT}"  # Dark
       if [[ ! -e "$LOGO_FILE" ]] ; then
-        f_log "==> Logo $LOGO_FILE nicht gefunden! Verwende 'Light'-Version."
+        f_log WARN "Logo $LOGO_FILE nicht gefunden! Verwende 'Light'-Version."
         LOGO_FILE="${MP_LOGODIR}/${MODE}/${FILE%.*}.${EXT}"  # Fallback auf Light
       fi
     fi
   else  # Normaler Modus mit PNG-Logos
     LOGO_FILE="${MP_LOGODIR}/${MODE}/${LOGO_VARIANT}/${FILE}"
   fi
-  [[ ! -e "$LOGO_FILE" ]] && { f_log "!!> Logo nicht gefunden! (${LOGO_FILE})" ; return ;}
+  [[ ! -e "$LOGO_FILE" ]] && { f_log WARN "Logo nicht gefunden! (${LOGO_FILE})" ; return ;}
   for channel in "${CHANNEL[@]}" ; do  # Einem Logo können mehrere Kanäle zugeordnet sein
     channel="${channel//\&amp;/\&}"    # HTML-Zeichen ersetzen
     if [[ "${TOLOWER:-ALL}" == 'ALL' ]] ; then
@@ -70,13 +70,13 @@ f_process_channellogo() {  # Verlinken der Senderlogos zu den gefundenen Kanäle
       if [[ "$channel" =~ / ]] ; then  # Kanal mit / im Namen
         CHANNEL_PATH="${channel%/*}"   # Der Teil vor dem lezten /
         mkdir --parents "${LOGODIR}/${CHANNEL_PATH}" || \
-          { f_log "Fehler: Verzeichnis \"${LOGODIR}/${CHANNEL_PATH}\" konnte nicht erstellt werden!" ; continue ;}
+          { f_log ERR "Verzeichnis \"${LOGODIR}/${CHANNEL_PATH}\" konnte nicht erstellt werden!" ; continue ;}
       fi
       if [[ "$USE_PLAIN_LOGO" == 'true' ]] ; then
-        f_log "Verlinke neue Datei (${FILE}) mit $channel" ; ((N_LOGO+=1))
+        f_log INFO "Verlinke neue Datei (${FILE}) mit $channel" ; ((N_LOGO+=1))
         # Symlink erstellen (--force überschreibt bereits existierenen Link)
         ln --force --symbolic "$LOGO_FILE" "${LOGODIR}/${channel}" || \
-          { f_log "Fehler: Symbolischer Link \"${LOGODIR}/${channel}\" konnte nicht erstellt werden!" ; continue ;}
+          { f_log ERR "Symbolischer Link \"${LOGODIR}/${channel}\" konnte nicht erstellt werden!" ; continue ;}
       else
         f_convert_logo "$LOGO_FILE" "$channel"  # Logo konvertiern und verlinken
       fi
@@ -94,6 +94,7 @@ f_convert_logo() {  # Logo konvertieren und verlinken
     f_log WARN "Hintergrund fehlt! (${SELF_PATH}/backgrounds/${resolution}/${background}.png)"
   fi
 
+  f_log INFO "Erstelle Logo ${logoname}…"
   # Erstelle Logo mit Hintergrund
   convert "${SELF_PATH}/backgrounds/${resolution}/${background}.png" \
     \( "$LOGO_FILE" -background none -bordercolor none -border 100 -trim -border 1% -resize "$resize" -gravity center -extent "$resolution" +repage \) \
@@ -101,7 +102,7 @@ f_convert_logo() {  # Logo konvertieren und verlinken
     | "$pngquant" - 2>> "$LOGFILE" > "${LOGODIR}/logos/${logoname}"
 
   ln --force --symbolic "${LOGODIR}/logos/${logoname}" "${LOGODIR}/${channel}" || \
-    { f_log "Fehler: Symbolischer Link \"${LOGODIR}/${channel}\" konnte nicht erstellt werden!" ; return 1 ;}
+    { f_log ERR "Symbolischer Link \"${LOGODIR}/${channel}\" konnte nicht erstellt werden!" ; return 1 ;}
 }
 
 f_element_in() {  # Der Suchstring ist das erste Element; der Rest das zu durchsuchende Array
@@ -119,7 +120,7 @@ while getopts ":c:" opt ; do
        if [[ -f "$CONFIG" ]] ; then  # Konfig wurde angegeben und existiert
          source "$CONFIG" ; CONFLOADED='Angegebene' ; break
        else
-         f_log "Fehler! Die angegebene Konfigurationsdatei fehlt! (\"${CONFIG}\")"
+         f_log ERR "Die angegebene Konfigurationsdatei fehlt! (\"${CONFIG}\")"
          exit 1
        fi
     ;;
@@ -139,27 +140,27 @@ if [[ -z "$CONFLOADED" ]] ; then  # Konfiguration wurde noch nicht geladen
     fi
   done
   if [[ -z "$CONFLOADED" ]] ; then  # Konfiguration wurde nicht gefunden
-    f_log "Fehler! Keine Konfigurationsdatei gefunden! (\"${CONFIG_DIRS[*]}\")"
+    f_log ERR "Keine Konfigurationsdatei gefunden! (\"${CONFIG_DIRS[*]}\")"
     exit 1
   fi
 fi
 
-f_log "==> $RUNDATE - $SELF_NAME #${VERSION} - Start..."
-f_log "$CONFLOADED Konfiguration: ${CONFIG}"
+f_log INFO "==> $RUNDATE - $SELF_NAME #${VERSION} - Start..."
+f_log INFO "$CONFLOADED Konfiguration: ${CONFIG}"
 if [[ "${LOGO_VARIANT:=Light}" == 'Simple' ]] ; then  # Leere oder veraltete Variable
-  f_log "!!!> Variable LOGO_VARIANT mit veralteten Wert 'Simple'!"
-  f_log "!!!> Verwende den Vorgabewert 'Light'. Bitte Konfiguration anpassen!"
+  f_log WARN "!!!> Variable LOGO_VARIANT mit veralteten Wert 'Simple'!"
+  f_log WARN "!!!> Verwende den Vorgabewert 'Light'. Bitte Konfiguration anpassen!"
   LOGO_VARIANT='Light'  # Vorgabewert setzen
 fi
 LOGO_VARIANT=".$LOGO_VARIANT"  # Anpassung an Ordnerstruktur im GIT
-[[ ! -e "$MP_LOGODIR" ]] && { f_log "==> Logo-Ordner (GIT) fehlt! (${MP_LOGODIR})" ; exit 1 ;}
-[[ ! -e "$LOGODIR" ]] && { f_log "==> Logo-Ordner fehlt! (${LOGODIR})" ; exit 1 ;}
+[[ ! -e "$MP_LOGODIR" ]] && { f_log ERR "Logo-Ordner (GIT) fehlt! (${MP_LOGODIR})" ; exit 1 ;}
+[[ ! -e "$LOGODIR" ]] && { f_log ERR "Logo-Ordner fehlt! (${LOGODIR})" ; exit 1 ;}
 
 # Kanallogos (GIT) aktualisieren
 cd "$MP_LOGODIR" || exit 1
 git pull >> "${LOGFILE:-/dev/null}"
 
-[[ ! -d "${MP_LOGODIR}/TV/${LOGO_VARIANT}" ]] && { f_log "Fehler: Ordner $LOGO_VARIANT fehlt!" ; exit 1 ;}
+[[ ! -d "${MP_LOGODIR}/TV/${LOGO_VARIANT}" ]] && { f_log ERR "Ordner $LOGO_VARIANT fehlt!" ; exit 1 ;}
 
 if [[ -z "${LOGO_CONF[*]}" ]] ; then
   USE_PLAIN_LOGO='true'
